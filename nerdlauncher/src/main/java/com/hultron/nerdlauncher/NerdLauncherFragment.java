f3ab15e5b33eed81d1d675afddd7d559baf562e5
Hultron <hultron@foxmail.com>
2017-02-19 13:27:38 +0800

    获取系统应用信息并显示

---
+++ nerdlauncher/src/main/java/com/hultron/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,10 @@
+package com.hultron.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Hultron on 2017/2/18.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+}
---
+++ nerdlauncher/src/main/java/com/hultron/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,8 @@
+package com.hultron.nerdlauncher;
+
+/**
+ * Created by Hultron on 2017/2/18.
+ */
+
+public class SingleFragmentActivity {
+}
