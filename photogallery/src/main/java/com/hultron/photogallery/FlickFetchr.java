0a7cb7de27580f312c13ce19c602fab15a50ad4b
Hultron <hultron@foxmail.com>
2017-02-20 12:47:07 +0800

    photogallery

---
+++ photogallery/src/main/java/com/hultron/photogallery/FlickFetchr.java
@@ -0,0 +1,10 @@
+package com.hultron.photogallery;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class FlickFetchr {
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+    }
+}
